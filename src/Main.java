4d243d9b302bc678cc3c813a77082364cbf2a07d
Koen Dorsthorst te <koentedorsthorst@gmail.com>
2021-12-03 17:57:51 +0100

    Made the first three problems of project euler

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import problems.Problem1;
+import problems.Problem2;
+import problems.Problem3;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        //Problem1 problem1 = new Problem1();
+        //problem1.solve();
+        //Problem2 problem2 = new Problem2();
+        //problem2.solve();
+        Problem3 problem3 = new Problem3();
+        problem3.solve();
+
+    }
+
+
+}
---
+++ src/problems/Problem3.java
@@ -0,0 +1,42 @@
+package problems;
+
+//Problem 3
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//
+//What is the largest prime factor of the number 600851475143 ?
+
+public class Problem3 {
+
+
+    public void solve() {
+        long factorNumber = 600851475143L;
+        long biggestFactor = 0;
+        long[] factors = new long[2];
+
+        for (long i = 2; i * i < factorNumber; i++) {
+            if (factorNumber % i == 0) {
+                if(checkIfPrime(i)){
+                    if(i > biggestFactor){
+                        biggestFactor = i;
+                    }
+                }
+                if(checkIfPrime(factorNumber / i)){
+                    if(factorNumber/i > biggestFactor){
+                        biggestFactor = factorNumber/i;
+                    }
+                }
+
+            }
+        }
+        System.out.print(biggestFactor);
+    }
+
+    boolean checkIfPrime(long number){
+        for (long i = 2; i < number; i++) {
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
