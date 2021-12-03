041248418605f78c6318e37e3927955c2d4a84b4
Koen Dorsthorst te <koentedorsthorst@gmail.com>
2021-12-04 00:28:00 +0100

    Solved problem 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import problems.Problem1;
 import problems.Problem2;
 import problems.Problem3;
+import problems.Problem4;
 
 public class Main {
 
@@ -10,8 +11,9 @@ public class Main {
         //problem1.solve();
         //Problem2 problem2 = new Problem2();
         //problem2.solve();
-        Problem3 problem3 = new Problem3();
-        problem3.solve();
+        //Problem3 problem3 = new Problem3();
+        //problem3.solve();
+        Problem4 problem4 = new Problem4();
 
     }
 
