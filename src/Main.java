c888226924a6f2e3da42f67d01311e06f1819711
Koen Dorsthorst te <koentedorsthorst@gmail.com>
2021-12-04 13:26:07 +0100

    Solved problem 8 (Not my best solution, mostly because I make the long string a char array, just to convert it back to a string and then to an integer.)

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Problem1 problem1 = new Problem1();
-        Problem2 problem2 = new Problem2();
-        Problem3 problem3 = new Problem3();
-        Problem4 problem4 = new Problem4();
-        Problem5 problem5 = new Problem5();
-        Problem6 problem6 = new Problem6();
-        Problem7 problem7 = new Problem7();
+//        Problem1 problem1 = new Problem1();
+//        Problem2 problem2 = new Problem2();
+//        Problem3 problem3 = new Problem3();
+//        Problem4 problem4 = new Problem4();
+//        Problem5 problem5 = new Problem5();
+//        Problem6 problem6 = new Problem6();
+//        Problem7 problem7 = new Problem7();
+        Problem8 problem8 = new Problem8();
 
     }
 
