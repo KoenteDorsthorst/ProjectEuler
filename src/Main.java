b7ecac258b263835f3004afe30daa035a1fe8ea0
Koen Dorsthorst te <koentedorsthorst@gmail.com>
2021-12-04 15:05:22 +0100

    Solved problem 9

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,8 @@ public class Main {
 //        Problem5 problem5 = new Problem5();
 //        Problem6 problem6 = new Problem6();
 //        Problem7 problem7 = new Problem7();
-        Problem8 problem8 = new Problem8();
+//        Problem8 problem8 = new Problem8();
+        Problem9 problem9 = new Problem9();
 
     }
 
